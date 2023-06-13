284587a0772fd73cc3a653e0f7bff7d2ab7bf5ce
ck9 <0926cookie@gmail.com>
2023-06-13 14:34:41 +0900

    update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+import os
 from flask import Flask, request, abort
+from dotenv import load_dotenv
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(this_dir, '.env'))
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -13,11 +18,18 @@ import os
 
 app = Flask(__name__)
 
-LINE_ACCESS_TOKEN = os.environ["LINE_ACCESS_TOKEN"]
-LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+# import logging
+# LOGFILE_NAME = "DEBUG.log"
+# app.logger.setLevel(logging.DEBUG)
+# log_handler = logging.FileHandler(LOGFILE_NAME)
+# log_handler.setLevel(logging.DEBUG)
+# app.logger.addHandler(log_handler)
 
-line_bot_api = LineBotApi(LINE_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
+line_access_token = os.environ.get("LINE_ACCESS_TOKEN")
+line_channel_secret = os.environ.get("LINE_CHANNEL_SECRET")
+
+line_bot_api = LineBotApi(line_access_token)
+handler = WebhookHandler(line_channel_secret)
 
 @app.route("/recycle-linebot", methods=['POST'])
 def callback():
@@ -48,6 +60,4 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
